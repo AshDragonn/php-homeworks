d839a334f13d62b1b27ddba402f98c4658959500
Homenko Varvara <varya2105@gmail.com>
2025-05-21 11:55:15 +0300

    edit ind2

--- index2.php
+++ index2.php
@@ -3,54 +3,50 @@
 // $variable = 3;
 // $variable = 'one';
 // $variable = true;
-$variable = null;
-// $variable = [];
+// $variable = null;
+$variable = [];
 
 $type = gettype($variable);
 if (is_bool($variable)) {
-echo "type is $type";
-}
-elseif (is_bool($variable) ) {
-echo "type is $type";
-}
-elseif (is_float($variable) ) {
-echo "type is $type";
-}
-elseif (is_int($variable)) {
-echo "type is $type";
-}
-elseif (is_string($variable)) {
-echo "type is $type";
-}
-elseif (is_null($variable)) {
-echo "type is $type";
-}
-else  {
-echo "other";
+    echo "type is $type";
+} elseif (is_bool($variable) ) {
+    echo "type is $type";
+} elseif (is_float($variable) ) {
+    echo "type is $type";
+} elseif (is_int($variable)) {
+    echo "type is $type";
+} elseif (is_string($variable)) {
+    echo "type is $type";
+} elseif (is_null($variable)) {
+    echo "type is $type";
+} else  {
+    echo "type is other";
 }
 echo "\n";
+
+
 switch (true) {
-    case 0:
-        $type === "boolean";
-        echo "type0 is $type";
+    case is_bool($variable):
+        $type2 = "boolean";
         break;
-    case 1:
-        $type === "NULL";
-        echo "type1 is $type";
+    case is_null($variable):
+        $type2 = "NULL";
         break;
-    case 2:
-        $type === "string";
-        echo "type2 is $type";
+    case is_string($variable):
+        $type2 = "string";
         break;
-    case 3:
-        $type === "integer";
-        echo "type3 is $type";
+    case is_int($variable):
+        $type2 = "integer";
         break;
-    case 4:
-        $type === "double";
-        echo "type4 is $type";
+    case is_float($variable):
+        $type2 = "float";
         break;
     default:
-        echo "5other";
+        $type2 = "other";
 }
+echo "type is $type2";
 
+// switch ($type) {
+//     case "boolean":
+//         echo "type10 is $type";
+//         break;
